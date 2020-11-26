84ec21aaf02a9a5f505cb6d5536f63c3f565c9da
onlywyj <wyj669666@outlook.com>
2020-11-26 10:22:45 +0800

    Baidu Map API Forward and Reverse address encoding

---
+++ src/main/java/wyj/grab_data/GrabDataApplication.java
@@ -0,0 +1,13 @@
+package wyj.grab_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrabDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrabDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wyj/grab_data/GrabDataApplicationTests.java
@@ -0,0 +1,13 @@
+package wyj.grab_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrabDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
